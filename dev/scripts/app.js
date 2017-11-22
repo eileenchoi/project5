f8420e1073fc1ec9c0df26005f212407421f7e90
Eileen choi <eileen.h.choi@gmail.com>
2017-11-21 19:16:29 -0500

    Adding Donuts

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const donuts = ['Sourcream glazed', 'Honey dip', 'Boston cream'];
+import Donut from './donut';
+
 
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          {donuts.map(donut => {
+            return (
+              <Donut donutName={donut} />
+            
+            )
+          })}
+          {/* <h1>List</h1>
+          <h2>list category</h2>
+          <form action="">
+              <input type="text"/>  
+              <button>submit</button>
+          </form> */}
         </div>
       )
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+//pseudo code
+//the user will type into the input and press enter
+  //if the input has text
+  //save the input from the form
+  //take the input, create a list item that contains whatever the user entered 
+  //put the new list item on the page in teh existing <ul>
+  //be able to remove items as needed
